529537fd4c102790b64097d0452f4dfac124ec0c
Kaviarasu <kavinskarasu@gmail.com>
2022-05-06 23:24:27 +0530

    modified

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ mongoose
   .then(() => console.log("DB connection successful!"));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("server is running on port 4000");
+  console.log(`server is running on port ${port} `);
 });
